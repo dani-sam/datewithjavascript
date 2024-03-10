9795725f02afbacb497bceac25326ed1c96f7d58
dani-sam <samdani295@gmail.com>
2024-03-10 23:15:27 +0530

    completed step2 & step3

--- project.js
+++ project.js
@@ -24,5 +24,38 @@ const despot = () => {
     }
 };
 
-const depositAmount = despot()
-console.log(depositAmount);
+const getNumberOfLines = ()=> {
+    while (true) {
+        const lines = prompt('Enter the number of lines to bet on (1-3): ');
+        const numberOfLines = parseFloat(lines);
+
+
+        if(isNaN(numberOfLines) || numberOfLines <= 0  || numberOfLines > 3)
+        {
+            console.log("Invalid number of lines ,try again.");
+        }else{
+            return numberOfLines;
+        }
+
+    }
+};
+
+const getBet = (balance,lines) => {
+    while (true) {
+        const bet = prompt('Enter the amount of bet per line: '); 
+        const numberBet = parseFloat(bet);
+
+
+        if(isNaN(numberBet) || numberBet <= 0  || numberBet > balance / lines)
+        {
+            console.log("Invalid bet ,try again.");
+        }else{
+            return numberOfLines;
+        }
+
+    }
+}
+
+let balance = despot()
+const numberOfLines = getNumberOfLines()
+const bet = getBet(balance,numberOfLines)
